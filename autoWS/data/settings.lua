4a9b0fd64d216798777a8963f65afa8be984f913
sebyg666 <sebyg666@hotmail.com>
2017-03-07 23:34:46 +0000

    adding new addons

---
+++ autoWS/data/settings.lua
@@ -0,0 +1,14 @@
+return {
+    ['Chitara'] = {
+        ['COR'] = {
+            ['Sword'] = {
+                ['hps'] = {
+                    ['<'] = 100,
+                    ['>'] = 0
+                },
+                ['mobs'] = {},
+                ['ws_cmd'] = '/ws "Leaden Salute" <t>'
+            }
+        }
+    }
+}
